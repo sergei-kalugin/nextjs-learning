412cf6fc33df1b5fba6ee8210de6357215e8734d
Sergei Kalugin <kalugin.serge@gmail.com>
2024-07-05 17:01:51 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "@/app/ui/global.scss";
+import { inter } from "@/app/ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${inter.className} antialiased`}>
+    <body>
+    <div className="test-div">
+      <h1 className="text-blue-500">Foo bar baz!</h1>
+    </div>
+
+    {children}
+    </body>
+    </html>
+  );
+}
