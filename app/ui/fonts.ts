8aedbbba778d08012472cbba234da8f25b189c1a
Sergei Kalugin <kalugin.serge@gmail.com>
2024-07-08 13:26:47 -0500

    Completes Chapter 3.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,5 +3,6 @@ import { Inter, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
   subsets: ['latin'],
-  weight: ['400', '700']
+  weight: ['400', '700'],
+  display: 'swap',
 });
