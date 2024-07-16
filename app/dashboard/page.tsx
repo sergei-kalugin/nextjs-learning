d8cc1a3e600d237da40fcb28d3ed458fddd3e99d
Sergei Kalugin <kalugin.serge@gmail.com>
2024-07-16 15:28:13 -0500

    Continues with Chapter 7

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {lusitana} from "@/app/ui/fonts";
-import {fetchRevenue} from "@/app/lib/data";
+import {fetchLatestInvoices, fetchRevenue} from "@/app/lib/data";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import {Revenue} from "@/app/lib/definitions";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 
 export default async function Page(): Promise<React.ReactElement> {
   const revenue = await fetchRevenue();
-
-  console.log(revenue.length);
+  const latestInvoices = await fetchLatestInvoices();
 
   return(
       <main>
@@ -16,9 +18,8 @@ export default async function Page(): Promise<React.ReactElement> {
           {/* Cards go here */}
         </div>
         <div className="mt-6 border-t-4 grid grid-cols-1 gap-6 md:grid-cols:4 lg:grid-cols-8">
-          Revenue Chart
-          <hr/>
-          Latest Invoices
+          <RevenueChart revenue={revenue}/>
+          <LatestInvoices latestInvoices={latestInvoices}/>
         </div>
       </main>
   )
