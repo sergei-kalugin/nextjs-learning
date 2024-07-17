f9ec9579834d5d651b4f8ab4d78536518609f57f
Sergei Kalugin <kalugin.serge@gmail.com>
2024-07-17 18:50:27 -0500

    Completes Chapter 10.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {Suspense} from "react";
+import {lusitana} from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import {CreateInvoice} from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
+import {InvoicesTableSkeleton} from "@/app/ui/skeletons";
 
-export default function Page(): React.ReactElement {
+export default async function Page(): Promise<React.ReactElement> {
   return (
-    <main>
-      <h1>Invoices page</h1>
-    </main>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <hr/>
+        <CreateInvoice />
+      </div>
+      {/*<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>*/}
+      {/*  <Table />*/}
+      {/*</Suspense>*/}
+      <div className="mt-5 flex w-full justify-center">
+        [Pagination here]
+        {/*<Pagination totalPages={totalPages} />*/}
+      </div>
+    </div>
   );
 }
