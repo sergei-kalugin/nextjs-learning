3af2e58631dae203bdd34e5b440a555e517ae475
Sergei Kalugin <kalugin.serge@gmail.com>
2024-07-17 14:02:03 -0500

    Completes Chapter 9.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,31 +1,24 @@
 import React, {Suspense} from "react";
 import {lusitana} from "@/app/ui/fonts";
-import {
-  fetchCardData,
-} from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import {Card} from "@/app/ui/dashboard/cards";
-import {InvoicesTableSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton} from "@/app/ui/skeletons";
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
+import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page(): Promise<React.ReactElement> {
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
-
   return(
       <main>
         <h1 className={`${lusitana.className} mb-4 text-2xl md:text-3xl`}>
           Dashboard
         </h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-           <Card title="Collected" value={totalPaidInvoices} type="collected" />
-           <Card title="Pending" value={totalPendingInvoices} type="pending" />
-           <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-          <Card title="Total Customers" value={numberOfCustomers} type="customers"/>
+          <Suspense fallback={<CardsSkeleton />}>
+            <CardWrapper />
+          </Suspense>
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols:4 lg:grid-cols-8">
           <Suspense fallback={<RevenueChartSkeleton />}>
