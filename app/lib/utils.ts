8e648705afe7a5865fbcfc0daca3f56f918b21a7
Sergei Kalugin <kalugin.serge@gmail.com>
2024-08-16 16:59:18 -0500

    Completes Chapter 16.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,6 @@
 import { Revenue } from './definitions';
+import {QueryResultRow} from "pg";
+import {Assume} from "drizzle-orm";
 
 export const formatCurrency = (amount: number) => {
   return (amount / 100).toLocaleString('en-US', {
@@ -21,11 +23,11 @@ export const formatDateToLocal = (
   return formatter.format(date);
 };
 
-export const generateYAxis = (revenue: Revenue[]) => {
+export const generateYAxis = (revenue: any) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
   const yAxisLabels = [];
-  const highestRecord = Math.max(...revenue.map((month) => month.revenue));
+  const highestRecord = Math.max(...revenue.map((month: any) => month.revenue));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
 
   for (let i = topLabel; i >= 0; i -= 1000) {
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -5,7 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
 import {fetchLatestInvoices} from "@/app/lib/data";
 export default async function LatestInvoices() {
-  const latestInvoices: LatestInvoice[] = await fetchLatestInvoices();
+  const latestInvoices: { [p: string]: any; amount: string }[] = await fetchLatestInvoices();
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
